f84f0caf3e03c6e026ccbc1dfd0a792f5b2695e9
alvaro <aejjimenez19@outlook.com>
2024-06-05 21:50:10 -0500

    Create project and conect to dataBase

---
+++ src/test/java/com/FacutraExpress/apiFactura/ApiFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FacutraExpress.apiFactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
